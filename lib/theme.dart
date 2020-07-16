610ac8b0e32d8e7dea6ee529fa1040e38a0b7af7
avendo <namankaushikplt@gmil.com>
2020-07-16 13:29:09 +0530

    cutdown

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  primarySwatch: Colors.white,
+  accentColor: Colors.yellowAccent,
+);
+
+ThemeData darkTheme = ThemeData(
+  primarySwatch: Colors.black,
+  accentColor: Colors.blueGrey,
+)
